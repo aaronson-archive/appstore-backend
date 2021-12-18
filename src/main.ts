3069a5efe405ee3a93400aaf4f3cef5c4c70bdb8
0xbono <iam@sonjim.in>
2021-12-18 16:40:03 +0900

    feat(main): LoggerService 및 CORS 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@app/app.module';
+import { LoggerService } from '@utils/logger';
+import { HttpExceptionFilter } from '@utils/exceptions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+declare const module: any;
+
+async function create() {
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    logger: new LoggerService(),
+  });
+
+  app.enableCors({
+    origin: ['http://localhost'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
-bootstrap();
+
+create();
