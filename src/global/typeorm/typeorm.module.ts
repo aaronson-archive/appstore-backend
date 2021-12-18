0aabfc1467689183447e8caea038eb041b452fd2
0xbono <iam@sonjim.in>
2021-12-18 16:51:03 +0900

    feat(global/typeorm): TypeORM 관련한 GlobalModule 구성

---
+++ src/global/typeorm/typeorm.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from '@global/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('typeorm.host'),
+        port: configService.get('typeorm.port'),
+        database: configService.get('typeorm.database'),
+        username: configService.get('typeorm.username'),
+        password: configService.get('typeorm.password'),
+        synchronize: configService.get('typeorm.synchronize'),
+        logging: configService.get('typeorm.logging'),
+        entities: [...entities],
+        bigNumberStrings: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class GlobalTypeOrmModule {}
