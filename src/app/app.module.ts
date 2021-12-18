c6a98145fb660637405f4f418f8ad16b0e3332c4
0xbono <iam@sonjim.in>
2021-12-18 17:01:30 +0900

    feat(global/jwt): Jwt 관련한 모듈 GlobalModule로 구성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { GlobalConfigurationModule } from '@global/config';
+import { GlobalJwtModule } from '@global/jwt';
 import { GlobalTypeOrmModule } from '@global/typeorm';
 
 @Module({
-  imports: [GlobalConfigurationModule, GlobalTypeOrmModule],
+  imports: [GlobalConfigurationModule, GlobalJwtModule, GlobalTypeOrmModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
